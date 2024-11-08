e841697767d78ac771a2d65aa71b1226d3765423
manolara <em.arabatzis@gmail.com>
2024-11-08 12:47:06 +0000

    remaps and aliases

--- stow/.config/nvim/lua/remap.lua
+++ stow/.config/nvim/lua/remap.lua
@@ -25,21 +25,21 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Nice window resizing
+vim.keymap.set('n', '<S-D-,>', '<c-w>5<')
+vim.keymap.set('n', '<S-D-.>', '<c-w>5>')
+vim.keymap.set('n', '<S-D-t>', '<C-W>+')
+vim.keymap.set('n', '<S-D-s>', '<C-W>-')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
