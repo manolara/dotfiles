448f5e632d1d23787af7bd3b09b5aeb3473619b4
manolara <em.arabatzis@gmail.com>
2024-11-10 02:05:32 +0000

    not sure if that does anything but sure hope so

--- stow/.config/nvim/lua/plugins/lspconfig.lua
+++ stow/.config/nvim/lua/plugins/lspconfig.lua
@@ -163,11 +163,7 @@ return {
             },
           },
         },
-        -- clangd = {},
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
@@ -179,7 +175,7 @@ return {
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
-          -- capabilities = {},
+          capabilities = {},
           settings = {
             Lua = {
               completion = {
