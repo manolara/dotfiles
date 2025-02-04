6fd8dcd7d911567ccc438af202768e1966853740
manolara <em.arabatzis@gmail.com>
2025-02-04 17:10:03 +0000

    make diffview colors nicer

--- stow/.config/nvim/lua/plugins/vague.lua
+++ stow/.config/nvim/lua/plugins/vague.lua
@@ -1,3 +1,12 @@
+function FixDiffHighlightColors()
+  vim.api.nvim_set_hl(0, 'DiffviewDiffAddAsDelete', { bg = '#431313' })
+  vim.api.nvim_set_hl(0, 'DiffDelete', { bg = '#431313' })
+  vim.api.nvim_set_hl(0, 'DiffviewDiffDelete', { bg = 'none', fg = '#000000' })
+  vim.api.nvim_set_hl(0, 'DiffAdd', { bg = '#1B3028' })
+  vim.api.nvim_set_hl(0, 'DiffChange', { bg = '#28304D' })
+  vim.api.nvim_set_hl(0, 'DiffText', { bg = '#424674' })
+end
+
 return {
   {
     'vague2k/vague.nvim',
@@ -12,6 +21,7 @@ return {
         },
       }
       vim.cmd.colorscheme 'vague'
+      FixDiffHighlightColors()
     end,
   },
 }
